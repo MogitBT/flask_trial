e36ef9bad1d4ca2e805d95d43311366f389f9c81
Mogit B T <btmogit@gmail.com>
2024-06-04 21:27:40 +0530

    Practising Jinja2

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def home():
-    return "Hello, World!"
 
+    color = "Red"
+    animal_one = "Fox"
+    animal_two = "Bear"
+    orange_amount = 10
+    apple_amount = 20
+    donate_amount = 5
+    first_name = "Mogit"
+    last_name = "B T"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug = True)
+    kywords = {
+        "color" : color,
+        "animal_one" : animal_one,
+        "animal_two" : animal_two,
+        "orange_amount" : orange_amount,
+        "apple_amount" : apple_amount,
+        "donate_amount" : donate_amount,
+        "first_name" : first_name,
+        "last_name" : last_name
+    }
+
+    return render_template("home.html", **kywords)
